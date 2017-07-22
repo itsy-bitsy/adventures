44b5598043bf2413c0d791b9f6588aa680855ef0
Bhanu Iyer <biyer@mmm.com>
2017-07-22 17:11:55 -0600

    removed docs folder, moved all source files  a level up

---
+++ dist/app/pages/bcs/idaho/idaho.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+    templateUrl: './idaho.component.html',
+    styleUrls: ['./idaho.component.css']
+})
+
+export class IdahoComponent {
+    public pageTitle: string = "This is the Idaho page";
+}
